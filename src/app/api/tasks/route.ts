dcc17b3cc18d47b7f6adaaf7b30395669f3c02d9
Aayman-star <aayman.khalid1@gmail.com>
2024-03-03 22:55:43 +0500

    adding backend logic

--- src/app/api/tasks/route.ts
+++ src/app/api/tasks/route.ts
@@ -1,8 +1,47 @@
-import { NextRequest,NextResponse } from "next/server";
+import { NextRequest, NextResponse } from "next/server";
+import { db, taskTable, Task, newTask } from "@/lib/schema";
+import { eq, and, asc } from "drizzle-orm";
+import { sql } from "@vercel/postgres";
 
-export const GET = async(request:NextRequest) =>{
-    return NextResponse.json({message:"This api was called"})
+/**The GET function is used to fetch data from the database . In this function I have defined a database table in the schema file and
+ * also inferred its datatype from InferSelectMdodel and simply imported it into the route.ts file. This way I don't have to manually define the
+ * data type. the res variable is receiving data from the table tasktable
+ */
 
-    
-}
+export const GET = async (request: NextRequest) => {
+  try {
+    await sql`CREATE TABLE IF NOT EXISTS tasktable(id serial,tasktext text,is_complete boolean,created_at timestamp)`;
+    const res: Array<Task> = await db
+      .select()
+      .from(taskTable)
+      .orderBy(asc(taskTable.id));
+    console.log(res);
+    if (res.length) {
+      return NextResponse.json({ todos: res });
+    } else {
+      return NextResponse.json({ message: "No todos at the moment" });
+    }
+  } catch (err) {
+    console.log((err as { message: string }).message);
+    /*throw new Error("Something went wrong");*/
+  }
+};
 
+export const POST = async (request: NextRequest) => {
+  const req: newTask = await request.json();
+  try {
+    if (req.tasktext) {
+      const res = await db.insert(taskTable).values({
+        tasktext: req.tasktext,
+        is_complete: false,
+        created_at: new Date(),
+      });
+      return NextResponse.json({ message: "Task added successfully" });
+    } else {
+      return NextResponse.json({ message: "Please enter a task" });
+      /*throw new Error("Please enter a task");*/
+    }
+  } catch (err) {
+    console.log((err as { message: string }).message);
+  }
+};
---
+++ src/lib/schema.ts
@@ -0,0 +1,25 @@
+import { drizzle } from "drizzle-orm/vercel-postgres";
+import { sql } from "@vercel/postgres";
+import {
+  pgTable,
+  serial,
+  text,
+  varchar,
+  boolean,
+  timestamp,
+} from "drizzle-orm/pg-core";
+
+import { InferSelectModel, InferInsertModel } from "drizzle-orm";
+export const taskTable = pgTable("tasktable", {
+  id: serial("id").primaryKey(),
+  tasktext: text("tasktext").notNull(),
+  is_complete: boolean("is_complete").default(false).notNull(),
+  created_at: timestamp("created_at").defaultNow().notNull(),
+});
+
+//Exporting the type of the single task...
+export type Task = InferSelectModel<typeof taskTable>;
+//Infering the type of the task to be added
+export type newTask = InferInsertModel<typeof taskTable>;
+// Use this object to send drizzle queries to your DB
+export const db = drizzle(sql);
