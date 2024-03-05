9eda4f91eafeeb050b92c9d9f7b8fb574514e6d5
Aayman-star <aayman.khalid1@gmail.com>
2024-03-05 16:10:48 +0500

    Created client side functions for deleting the task  and updating  the task status

--- src/app/data/data.ts
+++ src/app/data/data.ts
@@ -1,4 +1,4 @@
-//const BASE_URL = "http://localhost:3000";
+/**This file contains all client side api calls to the backend */
 
 type todo = {
   id: number;
@@ -14,6 +14,7 @@ export const fetchTasks = async () => {
     if (!response.ok) {
       throw new Error("HTTP status " + response.status);
     }
+    /**Extracting the todos array from the received data */
     const { todos } = await response.json();
     console.log(`THIS IS THE RECEIVED DAT`, todos);
     return todos;
@@ -40,17 +41,53 @@ export const sendTask = async (taskText: string) => {
   return data;
 };
 
-// Function to call the API and delete the todo item
 export const deleteTask = async (id: number) => {
+  console.log("IN THE DATA FILE", id);
   try {
-    // Call the DELETE API and pass the id as a query parameter
     const response = await fetch(`/api/tasks?id=${id}`, {
       method: "DELETE",
     });
-    console.log(response);
-    // Handle success (e.g., update state or UI)
+
+    if (!response.ok) {
+      throw new Error(`HTTP error! status: ${response.status}`);
+    }
+
+    const data = await response.json();
+
+    if (data.Message === "Data deleted successfully") {
+      console.log("Task deleted successfully");
+    } else {
+      console.error(data.Message);
+    }
   } catch (error) {
     console.error(error);
-    // Handle error (e.g., display error message)
+  }
+};
+
+// Function to toggle the 'is_complete' status of a task
+export const toggleTaskCompletion = async (task: todo) => {
+  try {
+    const response = await fetch("/api/tasks", {
+      // Replace '/api/task' with your actual API route
+      method: "PATCH",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        id: task.id,
+        tasktext: task.tasktext,
+        is_complete: task.is_complete,
+        created_at: task.created_at, // Include the created_at property from the task object
+      }),
+    });
+
+    if (!response.ok) {
+      throw new Error("Network response was not ok");
+    }
+
+    const result = await response.json();
+    console.log(result.message); // Log the message from the server
+  } catch (error) {
+    console.error("There was a problem with the fetch operation:", error);
   }
 };
