80f80fef43c37021744d1af10213178d2b851444
Aayman-star <aayman.khalid1@gmail.com>
2024-08-05 12:35:26 +0500

    updated the footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -2,7 +2,7 @@ const Footer = () => {
   return (
     <div>
       <div className="w-full md:max-w-6xl md:mx-auto py-2 border-t-[1px] text-center">
-        <p className="font-thin">Copyright © TaskWise 2023</p>
+        <p className="">Copyright © TaskWise 2023</p>
       </div>
     </div>
   );
